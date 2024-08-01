33be519b22712849a5041ac9be7454cb3d9d9314
Mel <little00travesure15@hotmail.com>
2024-07-31 22:28:32 -0300

    :p otro cambio

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public int health = 20;
+    public int damage = 10;
+
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            
+            Vector2 direction = (player.position - transform.position).normalized;
+            transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }   
+    
+}
